3453fa22e4414c6a331005129206173f59366482
aileene <aileene.arellano@willowtreeapps.com>
2025-04-23 15:38:52 -0700

    get data from api

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -22,13 +27,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="main">
-        <h1> Robofriends </h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="main">
+          <h1> Robofriends </h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const card = ({name, email, id}) => {
-  return(
-    <div div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img alt = 'robot photo' src={`https://robohash.org/${id}?200x200`} />
-        <div>
-          <h2>{name}</h2>
-          <p>{email}</p>
+const card = ({ name, email, id, username }) => {
+  return (
+    <div div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robot photo" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+        <p>{username}</p>
       </div>
     </div>
-
   );
-}
+};
 
 export default card;
