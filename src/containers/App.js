9f0d7ad71e80a22db45d601942ffd20293341014
aileene <aileene.arellano@willowtreeapps.com>
2025-08-27 14:51:32 -0700

    Fix naming

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "../components/CardList.js";
-import SearchBox from "../components/Searchbox.js";
+import Searchbox from "../components/Searchbox.js";
 import "./Apps.css";
 
 class App extends Component {
@@ -32,7 +32,7 @@ class App extends Component {
       return (
         <div className="main">
           <h1> Robofriends </h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <Searchbox searchChange={this.onSearchChange} />
           <CardList robots={filteredRobots} />
         </div>
       );
